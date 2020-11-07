fcd0e162a2428205f5ecb530fc77f9d75ea7b5b4
Ryan Namgung <ryannamgung@gmail.com>
2020-11-07 01:20:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ function getFirstSelector(selector)
 {
   return document.querySelector(selector);
 }
+
+function nestedTarget()
+{
+  return document.getElementById('#nested .target');
+}
+
+function deepestChild()
+{
+  const grandNode = document.getElementById('grand-node');
+  while(granNode.)
+}

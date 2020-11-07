76cf8a4b64af6c2c3fbe8ab4604cc2f3bb147342
Ryan Namgung <ryannamgung@gmail.com>
2020-11-07 01:53:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ function deepestChild()
 function increaseRankBy(n)
 {
   const rankedLists = document.querySelectorAll('.ranked-list');
-  for(int i = 0; i < rankedLists.length; i++)
-  {
-    let children = rankedLists[i].children;
-    for(int j = 0; j < children.length; j++)
-    {
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+
+    for (let i = 0, l = rankedLists.length; i < l; i++) {
+      let children = rankedLists[i].children;
+
+      for (let j = 0, k = children.length; j < k; j++) {
+        children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+      }
     }
-  }
 }
